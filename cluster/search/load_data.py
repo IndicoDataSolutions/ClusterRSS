099c9ea9de4be1f12d276eb7fc122fc000a2167f
Anne Carlson <anne_carlson@brown.edu>
2016-05-11 17:36:57 -0400

    working but slow version

--- cluster/search/load_data.py
+++ cluster/search/load_data.py
@@ -1,7 +1,7 @@
 import os, re, json
 import time
 from copy import deepcopy
-from datetime import date
+import datetime
 
 import indicoio
 from tqdm import tqdm
@@ -9,7 +9,7 @@ from picklable_itertools.extras import partition_all
 import concurrent.futures
 import pyexcel as pe
 import pyexcel.ext.xlsx
-from datetil.parser import parse as date_parse
+from dateutil.parser import parse as date_parse
 
 from .client import ESConnection
 from .schema import Document, INDEX
@@ -23,10 +23,11 @@ with open('cluster/search/sp500.txt') as f:
     SP_TICKERS = f.read().splitlines()
 
 def parse_obj_to_document(obj):
-    text = obj.get("text")
-    title = obj.get("title")
+    text = obj.get("description_text")
+    title = obj.get("name_post", obj.get("name_story"))
     link = obj.get("url_post", obj.get("url_story"))
     tags = [obj.get("name_categorie", obj.get("name_topics"))]
+    pub_date = obj.get("date_published", obj.get("date_published_story"))
 
     # Remove CSS & HTML residue
     text = re.sub(r"<[^>]*>", "", text)
@@ -40,51 +41,39 @@ def parse_obj_to_document(obj):
         link=link,
         tags=tags,
         length=len(text),
+        date=pub_date,
         summary=ENGLISH_SUMMARIZER.parse(text, sentences=3)
     )
 
 
 def not_in_sp500(document):
-    print re.compile(r("[^a-zA-Z]"+"T"+"[^a-zA-Z]")).search(document["text"])
-    re.compile(r("[^a-zA-Z]"+ticker+"[^a-zA-Z]").search(document["text"])
-    in_text = any([re.compile(r("[^a-zA-Z]"+ticker+"[^a-zA-Z]")).search(document["text"]) for ticker in SP_TICKERS])
-    in_title = any([re.compile(r("[^a-zA-Z]"+ticker+"[^a-zA-Z]")).search(document["title"]) for ticker in SP_TICKERS])
+    in_text = any([re.compile(r'[^a-zA-Z]'+ re.escape(ticker)+ r'[^a-zA-Z]').search(document["text"]) for ticker in SP_TICKERS])
+    in_title = any([re.compile(r'[^a-zA-Z]'+ re.escape(ticker)+ r'[^a-zA-Z]').search(document["title"]) for ticker in SP_TICKERS])
     return not(in_text or in_title)
 
 
 def not_relevant_and_recent(document):
     if not document["text"] or not document["title"]:
+        print 'lacking content'
         return True
-    pub_date = date_parse(document.get("date_published", document.get("date_published_story")))
+    pub_date = date_parse(document.get("date"))
     if pub_date < ONE_YEAR_AGO and not_in_sp500(document):
+        print 'not relevant or recent'
         return True
     return False
 
 
 def add_indico(documents, filename):
     new_documents = []
-    for i, document in enumerate(documents):
+    for i, document in tqdm(enumerate(documents)):
         new_doc = deepcopy(document)
-        text = new_doc.get("description_text")
-        new_doc["text"] = text
-        title = new_doc.get("name_post", new_doc.get("name_story"))
-        new_doc["title"] = title
-        if not_relevant_and_recent(document):
+        text = new_doc.get("text")
+        title = new_doc.get("title")
+        new_doc["indico"] = {}
+        if not_relevant_and_recent(new_doc):
             continue
         print i, filename, title
         try:
-            # text_analysis = indicoio.analyze_text([text, title], apis=[
-            #     "sentiment_hq",
-            #     "keywords",
-            # ])
-            # new_doc["indico"]["sentiment"] = text_analysis.get('sentiment_hq')[0]
-            # new_doc["indico"]["keywords"] = text_analysis.get('keywords')[0]
-            # new_doc["indico"]["title_sentiment"] = text_analysis.get('sentiment_hq')[1]
-            # new_doc["indico"]["title_keywords"] = text_analysis.get('keywords')[1]
-
-            # new_doc["indico"]["text_features"] = indicoio.text_features(text)
-            # new_doc["indico"]["title_text_features"] = indicoio.text_features(title)
-
             text_analysis = indicoio.analyze_text([text, title], apis=[
                 "sentiment_hq",
                 "keywords",
@@ -98,7 +87,7 @@ def add_indico(documents, filename):
             new_doc["indico"]["title_keywords"] = text_analysis.get('keywords')[1]
             new_doc["indico"]["title_people"] = text_analysis.get('people')[1]
             new_doc["indico"]["title_places"] = text_analysis.get('places')[1]
-            new_doc["indico"]["title_organization"] = text_analysis.get('organization')[1]
+            new_doc["indico"]["title_organizations"] = text_analysis.get('organizations')[1]
             new_doc["indico"]["title_text_features"] = text_analysis.get('text_features')[1]
 
 
@@ -106,7 +95,7 @@ def add_indico(documents, filename):
             new_doc["indico"]["keywords"] = text_analysis.get('keywords')[0]
             new_doc["indico"]["people"] = text_analysis.get('people')[0]
             new_doc["indico"]["places"] = text_analysis.get('places')[0]
-            new_doc["indico"]["organization"] = text_analysis.get('organization')[0]
+            new_doc["indico"]["organizations"] = text_analysis.get('organizations')[0]
             new_doc["indico"]["text_features"] = text_analysis.get('text_features')[0]
 
 
@@ -118,33 +107,41 @@ def add_indico(documents, filename):
 
     return new_documents
 
-def upload_data(data_dir, filename):
+def upload_data(es, current_dir):
     t0 = time.time()
 
     documents = []
-    data_file = os.path.join(data_dir, filename)
-    documents = pe.get_records(file_name=data_file)
-    # with open(data_file, 'rb') as f:
-    #     documents = [json.loads(l) for l in f.readlines()]
+    for subdir, dirs, files in os.walk(current_dir):
+        for filename in files:
+            data_file = os.path.join(current_dir, filename)
+            print data_file
+            lines = [line for line in pe.get_records(file_name=data_file) if line.get("description_text") and len(line.get("description_text")) > 600][:10]
+            print len(lines)
 
-    # print len(lines)
-    # documents = map(lambda x: parse_obj_to_document(json.loads(x)), lines)
-    # documents = filter(lambda doc: doc.link and "Service Unavailable" not in doc.text, documents)
 
-    clean_documents = add_indico(documents, filename)
+            documents = map(lambda x: parse_obj_to_document(x), lines)
+            documents = filter(lambda doc: doc.link and "Service Unavailable" not in doc.text, documents)
+            print 'raw docs'
 
-    data_dir = os.path.join(os.path.dirname(__file__), '../../entities/')
-    if clean_documents:
-        print "about to write", len(clean_documents), "documents to", data_dir
-        with open(data_dir+filename, 'w') as data_dump:
-            for document in clean_documents:
-                data_dump.write(json.dumps(document)+'\n')
+            clean_documents = add_indico(documents, filename)
+            print "added_indico"
 
-    print 'done - total time:', time.time() - t0
-    return True
+
+            data_dir = os.path.join(os.path.dirname(__file__), '../../backups/')
+            if clean_documents:
+                print "about to write", len(clean_documents), "documents to", data_dir
+                with open(data_dir+filename, 'w') as data_dump:
+                    for document in clean_documents:
+                        data_dump.write(json.dumps(document)+'\n')
+
+            print "written"
+            es.upload(clean_documents)
+
+            print 'done - total time:', time.time() - t0
+            return True
 
 
 if __name__ == "__main__":
     es = ESConnection("localhost:9200", index=INDEX)
-    DATA_FILE = os.path.join(os.path.dirname(__file__), '../quickie.ndjson')
+    DATA_FILE = os.path.join(os.path.dirname(__file__), '../../inputxl')
     upload_data(es, DATA_FILE)
