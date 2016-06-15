c1f401f87c044a3efe4265108f5afc835428251d
Chris Lee <sihrc.c.lee@gmail.com>
2016-06-15 15:02:01 -0400

    FIX: initial elasticsearch tests

--- tests/elasticsearch/test_elasticsearch_actions.py
+++ tests/elasticsearch/test_elasticsearch_actions.py
@@ -1,12 +1,12 @@
-import os, unittest
+import os, unittest, datetime
 
-import indicluster
-from indicluster.elasticsearch.schema import Document
-from indicluster.elasticsearch.client import ESConnection
+import cluster
+from cluster.search.schema import Document
+from cluster.search.client import ESConnection
 
 SCRIPTS_EXEC = os.path.abspath(
     os.path.join(
-        os.path.dirname(indicluster.__file__),
+        os.path.dirname(cluster.__file__),
         "..",
         "scripts",
         "run_elasticsearch_host.sh"
@@ -16,40 +16,41 @@ SCRIPTS_EXEC = os.path.abspath(
 INDEX="testing-index"
 DOCUMENT = Document(
     title="sample title abcd",
-    text="sample text abcd"
+    text="sample text abcd",
+    date=datetime.datetime.now()
 )
 HOST="localhost:9200"
 
 class TestLoadData(unittest.TestCase):
+    def setUp(self):
+        self.es = ESConnection(HOST, index=INDEX)
+
     def test_upload_document(self):
-        es = ESConnection(HOST, index=INDEX)
-        successes, errors = es.upload([DOCUMENT])
+        successes, errors = self.es.upload([DOCUMENT])
         self.assertEqual(successes, 1)
         self.assertFalse(errors)
 
     def test_search_document(self):
-        es = ESConnection(HOST, index=INDEX)
-        es.upload([DOCUMENT])
-        es.flush()
-        results = es.search("sample")
+        self.es.upload([DOCUMENT])
+        self.es.flush()
+        results = self.es.search("sample")
         self.assertEqual(len(results), 1)
 
     def test_tags_as_list(self):
-        es = ESConnection(HOST, index=INDEX)
         document = Document(
             title="title",
             text="text",
             tags=["tag1", "tag2"],
-            link="google.com"
+            link="google.com",
+            date=datetime.datetime.now()
         )
-        successes, errors = es.upload([document])
+        successes, errors = self.es.upload([document])
         self.assertEqual(successes, 1)
-        es.flush()
+        self.es.flush()
 
-        results = es.search("title")
+        results = self.es.search("title")
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0]["tags"], ["tag1", "tag2"])
 
     def tearDown(self):
-        es = ESConnection(HOST, index=INDEX)
-        es.delete()
+        self.es.delete()
