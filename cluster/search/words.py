e4fd92a48e7a48cd484a11b50c65d76635ec7e2c
Chris Lee <sihrc.c.lee@gmail.com>
2016-09-06 17:09:05 -0400

    REMOVED: search to replace with submodule

--- cluster/search/words.py
+++
@@ -1,7 +0,0 @@
-"""
-Word utils for text input
-"""
-def cross_reference(source, reference):
-    if type(source) is basestring:
-        source = source.split()
-    return list(set(source).intersection(reference))
